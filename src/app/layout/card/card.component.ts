1c8f10fe6d77a65627958ea21509659d14fa3483
Bati <bati.odon@gmail.com>
2018-06-19 19:04:24 -0500

    change anim

--- src/app/layout/card/card.component.ts
+++ src/app/layout/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { routerTransition } from '../../router.animations'
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+     animations :[routerTransition()]
 })
 export class CardComponent implements OnInit {
 
